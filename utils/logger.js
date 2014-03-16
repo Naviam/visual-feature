a3d38a2d8b77e9f145290fe871524cdaf503a2a3
Vitali Hatalski <v.hatalski@gmail.com>
2014-03-16 17:54:23 -0400

    logger with own levels and logentries destination

---
+++ utils/logger.js
@@ -0,0 +1,31 @@
+var logentries = require('node-logentries');
+
+var myCustomLevels = {
+    levels: { debug:0, info:1, warning:2, error:3, critical:4, emergency:5 },
+    colors: {
+      debug: 'blue',
+      info: 'green',
+      warning: 'yellow',
+      error: 'red',
+      critical: 'red',
+      emergency: 'red'
+    }
+  };
+
+var log = logentries.logger({
+	token: '49b3c292-089e-4d6e-a58c-9bfaddb3ac86',
+	levels: myCustomLevels.levels
+});
+
+// use as a winston transport
+var winston = require('winston');
+winston.setLevels(myCustomLevels.levels);
+winston.addColors(myCustomLevels.colors);
+log.winston(winston, { level: 'debug' });
+
+// display logentries errors in console.
+log.on('error', function(err) {
+	console.log('logentries error: ' + err);
+});
+
+module.exports = winston;
